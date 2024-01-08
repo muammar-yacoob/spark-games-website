5ab1fa211880f6f6da71fa356f6f5ece6c3a2dd2
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-01-08 23:26:08 +0000

    sweet alert added

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -1,12 +1,9 @@
 const serviceID = 'service_rzijtbs';
 const templateID = 'template_msg1xrm';
 const publicKey = 'nzkw5r6_a36Y9MEeI';
-var templateParams = {
-    name: 'James',
-    notes: 'Check this out!'
-};
- function sendEmail(event) {
-    // event.preventDefault();
+
+function sendEmail(event) {
+    event.preventDefault();
     console.log("Sending message...");
     var form = document.getElementById('contact-form');
     var formData = new FormData(form);
@@ -18,9 +15,23 @@ var templateParams = {
 
     // Send the form data using EmailJS
     emailjs.sendForm(serviceID, templateID, form, publicKey)
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-        }, function(error) {
-            console.log('FAILED...', error);
+    .then(function(response) {
+        Swal.fire({
+            title: 'Success!',
+            text: 'Email successfully sent!',
+            icon: 'success',
+            confirmButtonText: 'Cool',
+            timer: 5000 // Alert will close after 5 seconds
         });
+        console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Failed to send email: ' + error.text,
+            icon: 'error',
+            confirmButtonText: 'OK',
+            timer: 5000 // Alert will close after 5 seconds
+        });
+        console.log('FAILED...', error);
+    });
 }
