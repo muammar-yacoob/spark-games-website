098516ebc68ac40718f06f3aa0e9dcde6bfdb73f
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-01-08 22:14:56 +0000

    sending email using emailjs.js [wip]

---
+++ assets/js/emailjs.js
@@ -0,0 +1,25 @@
+const serviceID = 'Spark-Games-Website';
+const templateID = 'template_msg1xrm';
+const publicKey = 'nzkw5r6_a36Y9MEeI';
+var templateParams = {
+    name: 'James',
+    notes: 'Check this out!'
+};
+ function sendEmail() {
+    console.log("Sending message...");
+    var form = document.getElementById('contact-form');
+    var formData = new FormData(form);
+
+    // Log the form data for debugging
+    for(var pair of formData.entries()) {
+        console.log(pair[0]+ ': '+ pair[1]);
+    }
+
+    // Send the form data using EmailJS
+    emailjs.sendForm(serviceID, templateID, form, publicKey)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+}
