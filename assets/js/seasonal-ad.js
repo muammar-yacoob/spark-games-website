cf68d1edbbf572db82e6a4de9712750d05e42c65
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-10-12 21:08:03 +0100

    works

--- assets/js/seasonal-ad.js
+++ assets/js/seasonal-ad.js
@@ -1,11 +1,15 @@
 function updateSeasonalBanner() {
     const banners = {
         'winter': ['12', '01'],
+        'lunar-new-year': ['01', '02'],
         'spring': ['03', '04', '05'],
+        'may-madness': ['05'],
         'summer': ['06', '07', '08'],
-        'halloween': ['09', '10', '11'],
+        'back-to-school': ['08', '09'],
+        'autumn': ['09', '10'],
         'halloween': ['10'],
-        'blackfriday': ['11'],
+        'black-friday': ['11'],
+        'holiday': ['12']
     };
 
     const date = new Date();
@@ -21,5 +25,3 @@ function updateSeasonalBanner() {
 
     document.getElementById('seasonal-ad').src = `images/AffiliateBanners/${season}.png`;
 }
-
-document.addEventListener('DOMContentLoaded', updateSeasonalBanner);
