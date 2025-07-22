ddecb9ddd90360c911624e2d8bbe89a9d69b7b38
Yacoob <yacoob@spark-games.co.uk>
2025-07-22 16:55:24 +0100

    Refactor modal close button and file removal button: Updated styling for the close button with a new background image and hover effects. Enhanced file removal button to include an icon and improved overall button consistency and responsiveness.

--- assets/js/main.js
+++ assets/js/main.js
@@ -287,11 +287,11 @@
 				var $this = $(this);
 
 				// Close.
-					$('<div class="close">Close</div>')
-						.appendTo($this)
-						.on('click', function() {
-							location.hash = '';
-						});
+					// $('<div class="close">Close</div>')
+					// 	.appendTo($this)
+					// 	.on('click', function() {
+					// 		location.hash = '';
+					// 	});
 
 				// Prevent clicks from inside article from bubbling.
 					$this.on('click', function(event) {
